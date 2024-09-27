579eb5c52460a9287bdcb4a2898970f4600e7c12
Udavin Wijesundara <86962780+uwtechshow-official@users.noreply.github.com>
2024-09-27 17:18:48 +0530

    Update config.js

--- config.js
+++ config.js
@@ -6,8 +6,8 @@ function convertToBool(text, fault = 'true') {
 }
 
 module.exports = {
-    SESSION_ID: process.env.SESSION_ID || '6I8H2SKI#-dyEFbyJ6nAmq9RRO4qlLwgwZrgf_MppFnaNq8I1SB4', // Enter Your Session ID
-    MONGODB: process.env.MONGODB || 'mongodb+srv://spriky:1234@cluster0.sx9pb.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0',    // Enter Your MongoDB URL
+    SESSION_ID: process.env.SESSION_ID || '', // Enter Your Session ID
+    MONGODB: process.env.MONGODB || '',    // Enter Your MongoDB URL
     Owner: process.env.OwnerNumber || '94758900210',    // Enter Your Owner Number
     BotNumber: process.env.BotNumber || '94773366833'    // Enter Your Bot Number
 };
